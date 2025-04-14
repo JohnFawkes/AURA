3764c9c5491fdf3ecd82b43e64801f0146790f09
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-14 17:52:30 -0400

    add PosterSets, PosterItem, PosterSet, File, Movie, Season, and Episode interfaces for media organization

---
+++ frontend/src/types/posterSets.ts
@@ -0,0 +1,51 @@
+export interface PosterSets {
+	Type?: string;
+	Item?: PosterItem;
+	Sets?: PosterSet[];
+}
+
+export interface PosterItem {
+	ID: string;
+	Title: string;
+	Status: string;
+	Tagline: string;
+	Slug: string;
+	DateUpdated?: string;
+	TvdbID?: string;
+	ImdbID?: string;
+	TraktID?: string;
+	FirstAirDate?: string;
+	ReleaseDate?: string;
+}
+
+export interface PosterSet {
+	ID: string;
+	User: {
+		Name: string;
+	};
+	DateCreated: string;
+	DateUpdated: string;
+	Files: File[];
+}
+
+export interface File {
+	ID: string;
+	Type: string;
+	Movie?: Movie;
+	Season?: Season;
+	Episode?: Episode;
+}
+
+export interface Movie {
+	ID: string;
+}
+
+export interface Season {
+	Number?: number;
+}
+
+export interface Episode {
+	Title?: string;
+	Number?: number;
+	Season?: Season;
+}
