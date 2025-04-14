a75f3475f72a2085b4779ee92da7df58d56ddf62
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-14 17:14:41 -0400

    add health check endpoint to monitor server status

---
+++ backend/internal/routes/health/health_check.go
@@ -0,0 +1,18 @@
+package health
+
+import (
+	"net/http"
+	"poster-setter/internal/utils"
+	"time"
+)
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	// Get the start time
+	startTime := time.Now()
+
+	utils.SendJsonResponse(w, http.StatusInternalServerError, utils.JSONResponse{
+		Status:  "success",
+		Message: "Server is online",
+		Elapsed: utils.ElapsedTime(startTime),
+	})
+}
