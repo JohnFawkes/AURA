c126fd38e0cd4070c84c84441d6d0ff6e4f37d11
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-20 06:16:26 -0400

    fix: consolidate plex response structs

--- backend/internal/modals/PlexResponses.go
+++ backend/internal/modals/PlexResponses.go
@@ -74,3 +74,17 @@ type PlexDirectoryItem struct {
 		Path string `xml:"path,attr"`
 	} `xml:"Location"`
 }
+
+type PlexPhotosResponse struct {
+	XMLName xml.Name        `xml:"MediaContainer"`
+	Size    int             `xml:"size,attr"`
+	Photos  []PlexPhotoItem `xml:"Photo"`
+}
+
+type PlexPhotoItem struct {
+	Key       string `xml:"key,attr"`
+	RatingKey string `xml:"ratingKey,attr"`
+	Thumb     string `xml:"thumb,attr"`
+	Selected  int    `xml:"selected,attr"`
+	Provider  string `xml:"provider,attr,omitempty"`
+}
