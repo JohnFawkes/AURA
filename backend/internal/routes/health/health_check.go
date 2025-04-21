f48df182aad2386e4f917ade7dccd29daac0f23a
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-21 10:37:37 -0400

    fix: update health check response status to OK

--- backend/internal/routes/health/health_check.go
+++ backend/internal/routes/health/health_check.go
@@ -10,7 +10,7 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	// Get the start time
 	startTime := time.Now()
 
-	utils.SendJsonResponse(w, http.StatusInternalServerError, utils.JSONResponse{
+	utils.SendJsonResponse(w, http.StatusOK, utils.JSONResponse{
 		Status:  "success",
 		Message: "Server is online",
 		Elapsed: utils.ElapsedTime(startTime),
