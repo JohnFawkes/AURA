9f31ce588079e56abfcdefa5a02cac36bc3cc410
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-21 15:01:28 -0400

    feat: enhance Loader component with optional loading text display

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { CircularProgress, Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-const Loader: React.FC = () => {
+const Loader: React.FC<{
+	loadingText?: string;
+}> = ({ loadingText }) => {
 	return (
 		<Box
 			sx={{
 				display: "flex",
+				flexDirection: "column",
 				justifyContent: "center",
 				alignItems: "center",
-				height: "100vh",
+				height: "100vh", // Full viewport height
 			}}
 		>
-			<CircularProgress />
+			{loadingText && (
+				<Typography
+					variant="h6"
+					sx={{ fontWeight: 400, marginBottom: 2 }}
+				>
+					{loadingText}
+				</Typography>
+			)}
+			<CircularProgress size={50} thickness={4} />
 		</Box>
 	);
 };
