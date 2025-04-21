039376106e48448fc2c7e7124ad9c00cb6ad9dd0
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-21 15:05:29 -0400

    renamed page to be more consistent

---
+++ frontend/src/components/HomeLibrarySelect.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+	Select,
+	MenuItem,
+	InputLabel,
+	FormControl,
+	Box,
+	Chip,
+	OutlinedInput,
+} from "@mui/material";
+import { LibrarySection } from "../types/mediaItem";
+import { Theme, useTheme } from "@mui/material/styles";
+import { SelectChangeEvent } from "@mui/material";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+	PaperProps: {
+		style: {
+			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+			width: 250,
+		},
+	},
+};
+
+interface LibrarySelectProps {
+	filteredListOptions: LibrarySection[];
+	selectedLibrary: string[];
+	onLibraryChange: (value: string[]) => void;
+}
+
+function getStyles(
+	name: string,
+	selectedLibraries: readonly string[],
+	theme: Theme
+) {
+	return {
+		fontWeight: selectedLibraries.includes(name)
+			? theme.typography.fontWeightMedium
+			: theme.typography.fontWeightRegular,
+	};
+}
+
+const HomeLibrarySelect: React.FC<LibrarySelectProps> = ({
+	filteredListOptions,
+	selectedLibrary,
+	onLibraryChange,
+}) => {
+	const theme = useTheme();
+
+	const handleChange = (event: SelectChangeEvent<string[]>) => {
+		const {
+			target: { value },
+		} = event;
+		onLibraryChange(
+			typeof value === "string" ? value.split(",") : (value as string[])
+		);
+	};
+
+	return (
+		<FormControl sx={{ mt: 2, mx: 0 }} fullWidth>
+			<InputLabel id="library-select-label">
+				Filter by Library Name
+			</InputLabel>
+			<Select
+				labelId="library-select-label"
+				id="library-select"
+				multiple
+				value={selectedLibrary}
+				onChange={handleChange}
+				input={
+					<OutlinedInput
+						id="select-multiple-chip"
+						label="Filter by Library Name"
+					/>
+				}
+				renderValue={(selected) => (
+					<Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+						{selected.map((value) => (
+							<Chip key={value} label={value} />
+						))}
+					</Box>
+				)}
+				MenuProps={MenuProps}
+			>
+				{filteredListOptions.map((section) => (
+					<MenuItem
+						key={section.ID}
+						value={section.Title}
+						style={getStyles(section.Title, selectedLibrary, theme)}
+					>
+						{section.Title}
+					</MenuItem>
+				))}
+			</Select>
+		</FormControl>
+	);
+};
+
+export default HomeLibrarySelect;
