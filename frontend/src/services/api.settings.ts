f369a169b3428713474d8e9798d3770b546f9bd9
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-21 15:43:03 -0400

    fix: unify error handling in fetchMediuxSets and fetchConfig functions

--- frontend/src/services/api.settings.ts
+++ frontend/src/services/api.settings.ts
@@ -1,16 +1,14 @@
 import apiClient from "./apiClient";
 import { APIResponse } from "../types/apiResponse";
 import { AppConfig } from "../types/config";
+import { ReturnErrorMessage } from "./api.shared";
 
 export const fetchConfig = async (): Promise<APIResponse<AppConfig>> => {
 	try {
 		const response = await apiClient.get<APIResponse<AppConfig>>(`/config`);
 		return response.data;
-	} catch {
-		return {
-			status: "error",
-			message: "Failed to fetch data from Config API",
-		};
+	} catch (error) {
+		return ReturnErrorMessage<AppConfig>(error);
 	}
 };
 
@@ -18,10 +16,7 @@ export const fetchLogContents = async (): Promise<APIResponse<string>> => {
 	try {
 		const response = await apiClient.get<APIResponse<string>>(`/logs`);
 		return response.data;
-	} catch {
-		return {
-			status: "error",
-			message: "Failed to fetch data from Logs API",
-		};
+	} catch (error) {
+		return ReturnErrorMessage<string>(error);
 	}
 };
