100701a42a574cb61a722abca7cb1483d3a010ff
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-04-29 22:43:47 -0400

    feat: integrate Zustand middleware for persistent state management

--- frontend/src/lib/setStore.ts
+++ frontend/src/lib/setStore.ts
@@ -1,6 +1,7 @@
 import { MediaItem } from "@/types/mediaItem";
 import { PosterSet } from "@/types/posterSets";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 // Zustand store type
 interface PosterMediaStore {
@@ -11,10 +12,17 @@ interface PosterMediaStore {
 	clear: () => void;
 }
 
-export const usePosterMediaStore = create<PosterMediaStore>((set) => ({
-	posterSet: null,
-	mediaItem: null,
-	setPosterSet: (posterSet) => set({ posterSet }),
-	setMediaItem: (mediaItem) => set({ mediaItem }),
-	clear: () => set({ posterSet: null, mediaItem: null }),
-}));
+export const usePosterMediaStore = create<PosterMediaStore>()(
+	persist(
+		(set) => ({
+			posterSet: null,
+			mediaItem: null,
+			setPosterSet: (posterSet) => set({ posterSet }),
+			setMediaItem: (mediaItem) => set({ mediaItem }),
+			clear: () => set({ posterSet: null, mediaItem: null }),
+		}),
+		{
+			name: "poster-media-storage", // key in localStorage
+		}
+	)
+);
