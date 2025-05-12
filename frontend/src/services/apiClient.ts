39f36becb36848146c357d3bcdd3e2fb53b312a1
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-05-12 19:52:45 -0400

    Fix: Timeout issues with large libraries

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
 	baseURL: "/api",
-	//timeout: 3000000, // 120 seconds
+	timeout: 3000000,
 	headers: {
 		"Content-Type": "application/json",
 	},
