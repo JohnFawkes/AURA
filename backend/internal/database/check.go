ca4ba2fee433b0c69082fe9ea276058f18a4a5b3
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-05-16 15:23:07 -0400

    Rename function to CheckIfMediaItemAlreadyInDatabase and update SQL query to reference Media_Item table

--- backend/internal/database/check.go
+++ backend/internal/database/check.go
@@ -2,11 +2,28 @@ package database
 
 import "poster-setter/internal/logging"
 
-func CheckIfAlreadyInDatabase(ratingKey string) (bool, logging.ErrorLog) {
+func CheckIfMediaItemAlreadyInDatabase(ratingKey string) (bool, logging.ErrorLog) {
 	query := `
-SELECT COUNT(*) FROM auto_downloader WHERE id = ?`
+SELECT COUNT(*) FROM Media_Item WHERE id = ?`
 	var count int
 	err := db.QueryRow(query, ratingKey).Scan(&count)
+	if err != nil {
+		return false, logging.ErrorLog{Err: err, Log: logging.Log{
+			Message: "Failed to query database",
+		}}
+	}
+	if count > 0 {
+		return true, logging.ErrorLog{}
+	}
+
+	return false, logging.ErrorLog{}
+}
+
+func CheckIfPosterSetAlreadyInDatabase(setID string) (bool, logging.ErrorLog) {
+	query := `
+SELECT COUNT(*) FROM Poster_Sets WHERE id = ?`
+	var count int
+	err := db.QueryRow(query, setID).Scan(&count)
 	if err != nil {
 		return false, logging.ErrorLog{Err: err, Log: logging.Log{
 			Message: "Failed to query database",
