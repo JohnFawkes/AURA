d27a186edf5ea64246dbc8f3eaee770ff398d968
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-05-16 15:23:35 -0400

    Refactor GetAllItemsFromDatabase to improve query structure and error handling for Media_Item and Poster_Sets

--- backend/internal/database/get_all_items.go
+++ backend/internal/database/get_all_items.go
@@ -28,29 +28,34 @@ func GetAllItems(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func GetAllItemsFromDatabase() ([]modals.ClientMessage, logging.ErrorLog) {
-	query := `
-SELECT media_item, poster_set, selected_types, auto_download, last_update FROM auto_downloader`
-	rows, err := db.Query(query)
+func GetAllItemsFromDatabase() ([]modals.Database_SavedSet, logging.ErrorLog) {
+
+	mediaQuery := "SELECT id, media_item FROM Media_Item"
+	mediaRows, err := db.Query(mediaQuery)
 	if err != nil {
 		return nil, logging.ErrorLog{Err: err, Log: logging.Log{
-			Message: "Failed to query database",
+			Message: "Failed to query Media_Item table",
 		}}
 	}
-	defer rows.Close()
-	var items []modals.ClientMessage
-	for rows.Next() {
-		var mediaItemJSON, setJSON, selectedTypes string
-		var autoDownload bool
-		var lastUpdate string
-		err := rows.Scan(&mediaItemJSON, &setJSON, &selectedTypes, &autoDownload, &lastUpdate)
+	defer mediaRows.Close()
+
+	var savedSets []modals.Database_SavedSet
+	for mediaRows.Next() {
+		var mediaItemID string
+		var mediaItemJSON string
+		err := mediaRows.Scan(&mediaItemID, &mediaItemJSON)
 		if err != nil {
 			return nil, logging.ErrorLog{Err: err, Log: logging.Log{
-				Message: "Failed to scan row",
+				Message: "Failed to scan Media_Item row",
 			}}
 		}
 
-		// Unmarshal the JSON data
+		savedSet := modals.Database_SavedSet{
+			ID:            mediaItemID,
+			MediaItemJSON: mediaItemJSON,
+		}
+
+		// Unmarshal the JSON data into modals.MediaItem struct
 		var mediaItem modals.MediaItem
 		err = json.Unmarshal([]byte(mediaItemJSON), &mediaItem)
 		if err != nil {
@@ -58,30 +63,71 @@ SELECT media_item, poster_set, selected_types, auto_download, last_update FROM a
 				Message: "Failed to unmarshal MediaItem data",
 			}}
 		}
+		savedSet.MediaItem = mediaItem
 
-		var set modals.PosterSet
-		err = json.Unmarshal([]byte(setJSON), &set)
+		posterQuery := `
+        SELECT id, media_item_id, poster_set, selected_types, auto_download, last_update
+        FROM Poster_Sets
+        WHERE media_item_id = ?`
+		posterRows, err := db.Query(posterQuery, mediaItemID)
 		if err != nil {
 			return nil, logging.ErrorLog{Err: err, Log: logging.Log{
-				Message: "Failed to unmarshal Set data",
+				Message: "Failed to query Poster_Sets table",
 			}}
 		}
 
-		selectedTypesSlice := strings.Split(selectedTypes, ",")
+		var sets []modals.Database_Set
+		for posterRows.Next() {
+			var setID, mediaItemID, setJSON, selectedTypesStr string
+			var autoDownload bool
+			var lastUpdate string
+			err := posterRows.Scan(&setID, &mediaItemID, &setJSON, &selectedTypesStr, &autoDownload, &lastUpdate)
+			if err != nil {
+				return nil, logging.ErrorLog{Err: err, Log: logging.Log{
+					Message: "Failed to scan Poster_Sets row",
+				}}
+			}
 
-		clientMessage := modals.ClientMessage{
-			MediaItem:     mediaItem,
-			Set:           set,
-			LastUpdate:    lastUpdate,
-			SelectedTypes: selectedTypesSlice,
-			AutoDownload:  autoDownload,
+			// Convert the comma-separated string into []string.
+			var selectedTypes []string
+			if selectedTypesStr != "" {
+				selectedTypes = strings.Split(selectedTypesStr, ",")
+			}
+
+			// Unmarshal the JSON data into modals.PosterSet struct
+			var set modals.PosterSet
+			err = json.Unmarshal([]byte(setJSON), &set)
+			if err != nil {
+				return nil, logging.ErrorLog{Err: err, Log: logging.Log{
+					Message: "Failed to unmarshal PosterSet data",
+				}}
+			}
+			dbSet := modals.Database_Set{
+				ID:            setID,
+				MediaItemID:   mediaItemID,
+				Set:           set,
+				SetJSON:       setJSON,
+				SelectedTypes: selectedTypes,
+				AutoDownload:  autoDownload,
+				LastUpdate:    lastUpdate,
+			}
+			sets = append(sets, dbSet)
 		}
-		items = append(items, clientMessage)
+		posterRows.Close()
+
+		savedSet.Sets = sets
+		savedSets = append(savedSets, savedSet)
 	}
-	if err = rows.Err(); err != nil {
+	if err = mediaRows.Err(); err != nil {
 		return nil, logging.ErrorLog{Err: err, Log: logging.Log{
-			Message: "Error iterating rows",
+			Message: "Error iterating Media_Item rows",
 		}}
 	}
-	return items, logging.ErrorLog{}
+	if err = mediaRows.Close(); err != nil {
+		return nil, logging.ErrorLog{Err: err, Log: logging.Log{
+			Message: "Error closing Media_Item rows",
+		}}
+	}
+	return savedSets, logging.ErrorLog{}
+
 }
