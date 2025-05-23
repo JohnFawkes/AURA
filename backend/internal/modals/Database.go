f4a8b2d322fc25b49acc8a5870c70c99e221177b
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-05-22 23:10:43 -0400

    Refactor database models to enhance structure and clarity for media items and poster sets

--- backend/internal/modals/Database.go
+++ backend/internal/modals/Database.go
@@ -1,19 +1,32 @@
 package modals
 
-type Database_SavedSet struct {
-	ID            string         `json:"ID"`
-	MediaItem     MediaItem      `json:"MediaItem"`
-	MediaItemJSON string         `json:"MediaItemJSON"`
-	Sets          []Database_Set `json:"Sets"`
+type DBSavedItem struct {
+	MediaItemID    string    `json:"MediaItemID"`    // ID of the Item from the Media Server
+	MediaItem      MediaItem `json:"MediaItem"`      // MediaItem
+	MediaItemJSON  string    `json:"MediaItemJSON"`  // JSON of the MediaItem
+	PosterSetID    string    `json:"PosterSetID"`    // ID of the PosterSet
+	PosterSet      PosterSet `json:"PosterSet"`      // PosterSet
+	PosterSetJSON  string    `json:"PosterSetJSON"`  // JSON of the PosterSet
+	LastDownloaded string    `json:"LastDownloaded"` // Last Downloaded Date
+	SelectedTypes  []string  `json:"SelectedTypes"`  // Types of Posters/Backdrops/Season Posters/Titlecards
+	AutoDownload   bool      `json:"AutoDownload"`   // Auto Download
 }
 
-type Database_Set struct {
-	ID            string    `json:"ID"`
-	MediaItemID   string    `json:"MediaItemID"`
-	Set           PosterSet `json:"Set"`
-	SetJSON       string    `json:"SetJSON"`
-	SelectedTypes []string  `json:"SelectedTypes"`
-	AutoDownload  bool      `json:"AutoDownload"`
-	LastUpdate    string    `json:"LastUpdate,omitempty"`
-	ToDelete      bool      `json:"ToDelete,omitempty"`
+// PosterSetDetail groups poster set details per media item.
+type DBPosterSetDetail struct {
+	PosterSetID    string    `json:"PosterSetID"`
+	PosterSet      PosterSet `json:"PosterSet"`
+	PosterSetJSON  string    `json:"PosterSetJSON"`
+	LastDownloaded string    `json:"LastDownloaded"`
+	SelectedTypes  []string  `json:"SelectedTypes"`
+	AutoDownload   bool      `json:"AutoDownload"`
+	ToDelete       bool      `json:"ToDelete"` // Flag to indicate if the poster set should be deleted
+}
+
+// MediaItemWithPosterSets groups a media item with its poster sets.
+type DBMediaItemWithPosterSets struct {
+	MediaItemID   string              `json:"MediaItemID"`
+	MediaItem     MediaItem           `json:"MediaItem"`
+	MediaItemJSON string              `json:"MediaItemJSON"`
+	PosterSets    []DBPosterSetDetail `json:"PosterSets"`
 }
