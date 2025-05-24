ed4424e857a24b8f7eaa38ed179bb91855a3be47
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-05-24 13:32:06 -0400

    Add function to fetch Mediux user follow and hide data

--- frontend/src/services/api.mediux.ts
+++ frontend/src/services/api.mediux.ts
@@ -3,6 +3,7 @@ import { APIResponse } from "../types/apiResponse";
 import { PosterSet } from "../types/posterSets";
 import { ReturnErrorMessage } from "./api.shared";
 import { log } from "@/lib/logger";
+import { MediuxUserFollowHide } from "@/types/mediuxUserFollowsHides";
 
 export const fetchMediuxSets = async (
 	tmdbID: string,
@@ -28,5 +29,25 @@ export const fetchMediuxSets = async (
 			}`
 		);
 		return ReturnErrorMessage<PosterSet[]>(error);
+	}
+};
+
+export const fetchMediuxUserFollowHides = async (): Promise<
+	APIResponse<MediuxUserFollowHide>
+> => {
+	log(`api.mediux - Fetching Mediux user follow/hide data started`);
+	try {
+		const response = await apiClient.get<APIResponse<MediuxUserFollowHide>>(
+			`/mediux/user/following_hiding`
+		);
+		log(`api.mediux - Fetching Mediux user follow/hide data completed`);
+		return response.data;
+	} catch (error) {
+		log(
+			`api.mediux - Fetching Mediux user follow/hide data failed with error: ${
+				error instanceof Error ? error.message : "Unknown error"
+			}`
+		);
+		return ReturnErrorMessage<MediuxUserFollowHide>(error);
 	}
 };
