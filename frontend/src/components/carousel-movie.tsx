accfafd100d81987ba53109ae97e4cdb284fd0c3
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-05-23 20:04:56 -0400

    Refactor CarouselMovie to integrate IDB search for TMDB IDs and improve poster opacity logic

--- frontend/src/components/carousel-movie.tsx
+++ frontend/src/components/carousel-movie.tsx
@@ -1,10 +1,56 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import { CarouselItem } from "@/components/ui/carousel";
 import { AssetImage } from "@/components/ui/asset-image";
 import { PosterFile, PosterSet } from "@/types/posterSets";
+import { searchIDBForTMDBID } from "../helper/searchIDBForTMDBID";
 
-export function CarouselMovie({ set }: { set: PosterSet }) {
+export function CarouselMovie({
+	set,
+	librarySection,
+}: {
+	set: PosterSet;
+	librarySection: string;
+}) {
+	const [idbResults, setIdbResults] = useState<{ [key: string]: boolean }>(
+		{}
+	);
+
+	useEffect(() => {
+		let isMounted = true;
+		if (set.OtherPosters) {
+			const fetchData = async () => {
+				const posterPromises = (set.OtherPosters ?? []).map(
+					async (poster) => {
+						if (!poster.Movie?.RatingKey) {
+							const result = await searchIDBForTMDBID(
+								poster.Movie?.ID || "",
+								librarySection
+							);
+							return { id: poster.ID, isInIDB: !!result };
+						}
+						return null;
+					}
+				);
+				const results = await Promise.all(posterPromises);
+				if (isMounted) {
+					const newResults: { [key: string]: boolean } = {};
+					results.forEach((res) => {
+						if (res) {
+							newResults[res.id] = res.isInIDB;
+						}
+					});
+					setIdbResults(newResults);
+				}
+			};
+			fetchData();
+		}
+		return () => {
+			isMounted = false;
+		};
+	}, [set.OtherPosters, librarySection]);
+
 	return (
 		<>
 			{/* Main Movie Poster and Backdrop */}
@@ -19,11 +65,7 @@ export function CarouselMovie({ set }: { set: PosterSet }) {
 								displayUser={true}
 								displayMediaType={true}
 								aspect="poster"
-								className={`w-full ${
-									!set.Poster.Movie?.RatingKey
-										? "opacity-35"
-										: ""
-								}`}
+								className={`w-full`}
 							/>
 						)}
 
@@ -33,18 +75,14 @@ export function CarouselMovie({ set }: { set: PosterSet }) {
 								displayUser={true}
 								displayMediaType={true}
 								aspect="backdrop"
-								className={`w-full ${
-									!set.Backdrop.Movie?.RatingKey
-										? "opacity-35"
-										: ""
-								}`}
+								className={`w-full`}
 							/>
 						)}
 					</div>
 				</CarouselItem>
 			)}
 
-			{/* Other Posters and Backdrops in Set*/}
+			{/* Other Posters and Backdrops in Set */}
 			{set.OtherPosters?.sort(
 				(a: PosterFile, b: PosterFile) =>
 					new Date(b.Movie?.ReleaseDate ?? 0).getTime() -
@@ -54,6 +92,7 @@ export function CarouselMovie({ set }: { set: PosterSet }) {
 					(backdrop: PosterFile) =>
 						backdrop.Movie?.ID === poster.Movie?.ID
 				);
+				const isInIDB = idbResults[poster.ID] || false;
 				return (
 					<CarouselItem key={`${set.ID}-other-${poster.ID}`}>
 						<div className="space-y-2">
@@ -63,7 +102,9 @@ export function CarouselMovie({ set }: { set: PosterSet }) {
 								displayMediaType={true}
 								aspect="poster"
 								className={`w-full ${
-									!poster.Movie?.RatingKey ? "opacity-35" : ""
+									!poster.Movie?.RatingKey && !isInIDB
+										? "opacity-35"
+										: ""
 								}`}
 							/>
 
