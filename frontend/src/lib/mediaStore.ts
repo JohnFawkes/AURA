0f2939d4b55abd49446fff94fe781773bdc1b2c8
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-06-06 14:47:24 -0400

    Refactor mediaStore to include hydration state management

--- frontend/src/lib/mediaStore.ts
+++ frontend/src/lib/mediaStore.ts
@@ -1,5 +1,4 @@
 import { MediaItem } from "@/types/mediaItem";
-
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -7,6 +6,8 @@ interface MediaStore {
 	mediaItem: MediaItem | null;
 	setMediaItem: (mediaItem: MediaItem) => void;
 	clear: () => void;
+	hasHydrated: boolean;
+	setHasHydrated: (hydrated: boolean) => void;
 }
 
 export const useMediaStore = create<MediaStore>()(
@@ -15,9 +16,14 @@ export const useMediaStore = create<MediaStore>()(
 			mediaItem: null,
 			setMediaItem: (mediaItem) => set({ mediaItem }),
 			clear: () => set({ mediaItem: null }),
+			hasHydrated: false,
+			setHasHydrated: (hydrated) => set({ hasHydrated: hydrated }),
 		}),
 		{
-			name: "media-item-storage", // key in localStorage
+			name: "media-item-storage",
+			onRehydrateStorage: () => (state) => {
+				state?.setHasHydrated(true);
+			},
 		}
 	)
 );
