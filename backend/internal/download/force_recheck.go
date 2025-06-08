3c7c511d08746baf1b847bc5a9aad0c9ce4b879c
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-06-08 17:29:39 -0400

    Refactor ForceRecheckItem to retrieve the latest item from the database before checking for autodownload

--- backend/internal/download/force_recheck.go
+++ backend/internal/download/force_recheck.go
@@ -1,10 +1,12 @@
 package download
 
 import (
+	"aura/internal/database"
 	"aura/internal/logging"
 	"aura/internal/modals"
 	"aura/internal/utils"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -26,7 +28,34 @@ func ForceRecheckItem(w http.ResponseWriter, r *http.Request) {
 	}
 
 	item := requestBody.Item
-	warningMessages := CheckItemForAutodownload(item)
+
+	// Get the latest item from DB incase it has been updated
+	allItems, logErr := database.GetAllItemsFromDatabase()
+	if logErr.Err != nil {
+		logging.LOG.ErrorWithLog(logErr)
+		utils.SendErrorJSONResponse(w, http.StatusInternalServerError, logErr)
+		return
+	}
+
+	// Find the item in the database
+	var dbSavedItem modals.DBMediaItemWithPosterSets
+	for _, dbItem := range allItems {
+		if dbItem.MediaItemID == item.MediaItemID {
+			dbSavedItem = dbItem
+			break
+		}
+	}
+	if dbSavedItem.MediaItemID == "" {
+		logErr := logging.ErrorLog{
+			Err: fmt.Errorf("item with ID %s not found in database", item.MediaItemID),
+			Log: logging.Log{Message: "Item not found in database", Elapsed: utils.ElapsedTime(startTime)},
+		}
+		logging.LOG.ErrorWithLog(logErr)
+		utils.SendErrorJSONResponse(w, http.StatusNotFound, logErr)
+		return
+	}
+
+	warningMessages := CheckItemForAutodownload(dbSavedItem)
 
 	if len(warningMessages) > 0 {
 		utils.SendJsonResponse(w, http.StatusOK, utils.JSONResponse{
@@ -39,7 +68,6 @@ func ForceRecheckItem(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// If no warnings, send a success response
-
 	utils.SendJsonResponse(w, http.StatusOK, utils.JSONResponse{
 		Status:  "success",
 		Message: "Force recheck completed successfully",
