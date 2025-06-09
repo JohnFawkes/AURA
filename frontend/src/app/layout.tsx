0f5ad837a7e6d86abb1c40eda866dd8d1e86af91
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-06-09 18:06:06 -0400

    code cleanup: new location for theme provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "sonner";
 
 import AppFooter from "@/components/layout/app-footer";
 import Navbar from "@/components/layout/navbar";
 import { JumpToTop } from "@/components/shared/buttons/jump-to-top";
-import { ThemeProvider } from "@/components/theme-provider";
 
 import { gabarito } from "../../public/fonts/Gabarito";
 import "./globals.css";
