db768f954e7e4538c880414d9fc721778655d41b
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-06-09 16:46:52 -0400

    refactor: update import path for JumpToTop component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from "@/components/theme-provider";
 import AppFooter from "@/components/ui/app-footer";
-import { JumpToTop } from "@/components/ui/jump-to-top";
+import { JumpToTop } from "@/components/shared/buttons/jump-to-top";
 import Navbar from "@/components/ui/navbar";
 import { Toaster } from "sonner";
 import "./globals.css";
