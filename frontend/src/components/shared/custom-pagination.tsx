713884f0560a35a1072b65d5991267a3fd055f6a
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-06-15 22:44:50 -0400

    refactor: streamline CustomPagination component structure for improved readability and maintainability

--- frontend/src/components/shared/custom-pagination.tsx
+++ frontend/src/components/shared/custom-pagination.tsx
@@ -31,70 +31,68 @@ export function CustomPagination({
 
 	return (
 		<div className="flex justify-center mt-8">
-			<Pagination>
-				<PaginationContent>
-					{/* Previous Page Button */}
-					{totalPages > 1 && currentPage > 1 && (
-						<PaginationItem>
-							<PaginationPrevious
-								onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
-							/>
-						</PaginationItem>
-					)}
-
-					{/* Page Input */}
-					<PaginationItem>
-						<div className="flex items-center gap-2">
-							<Input
-								type="number"
-								min={1}
-								max={totalPages}
-								value={currentPage}
-								onChange={(e) => {
-									const value = parseInt(e.target.value);
-									if (!isNaN(value) && value >= 1 && value <= totalPages) {
-										handlePageChange(value);
-									}
-								}}
-								className="w-16 h-8 text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-								onBlur={(e) => {
-									const value = parseInt(e.target.value);
-									if (isNaN(value) || value < 1) {
-										handlePageChange(1);
-									} else if (value > totalPages) {
-										handlePageChange(totalPages);
-									}
-								}}
-							/>
-						</div>
-					</PaginationItem>
-
-					{/* Next Page Button */}
-					{totalPages > 1 && currentPage < totalPages && (
-						<PaginationItem>
-							<PaginationNext
-								onClick={() =>
-									handlePageChange(Math.min(currentPage + 1, totalPages))
-								}
-							/>
-						</PaginationItem>
-					)}
-
-					{/* Ellipsis and End Page */}
-					{totalPages > 3 && currentPage < totalPages - 1 && (
-						<>
+			{totalPages > 1 && (
+				<Pagination>
+					<PaginationContent>
+						{/* Previous Page Button */}
+						{totalPages > 1 && currentPage > 1 && (
 							<PaginationItem>
-								<PaginationEllipsis />
+								<PaginationPrevious onClick={() => handlePageChange(Math.max(currentPage - 1, 1))} />
 							</PaginationItem>
+						)}
+
+						{/* Page Input */}
+						<PaginationItem>
+							<div className="flex items-center gap-2">
+								<Input
+									type="number"
+									min={1}
+									max={totalPages}
+									value={currentPage}
+									onChange={(e) => {
+										const value = parseInt(e.target.value);
+										if (!isNaN(value) && value >= 1 && value <= totalPages) {
+											handlePageChange(value);
+										}
+									}}
+									className="w-16 h-8 text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+									onBlur={(e) => {
+										const value = parseInt(e.target.value);
+										if (isNaN(value) || value < 1) {
+											handlePageChange(1);
+										} else if (value > totalPages) {
+											handlePageChange(totalPages);
+										}
+									}}
+								/>
+							</div>
+						</PaginationItem>
+
+						{/* Next Page Button */}
+						{totalPages > 1 && currentPage < totalPages && (
 							<PaginationItem>
-								<PaginationLink onClick={() => handlePageChange(totalPages)}>
-									{totalPages}
-								</PaginationLink>
+								<PaginationNext
+									onClick={() => handlePageChange(Math.min(currentPage + 1, totalPages))}
+								/>
 							</PaginationItem>
-						</>
-					)}
-				</PaginationContent>
-			</Pagination>
+						)}
+
+						{/* Ellipsis and End Page */}
+						{totalPages > 3 && currentPage < totalPages - 1 && (
+							<>
+								<PaginationItem>
+									<PaginationEllipsis />
+								</PaginationItem>
+								<PaginationItem>
+									<PaginationLink onClick={() => handlePageChange(totalPages)}>
+										{totalPages}
+									</PaginationLink>
+								</PaginationItem>
+							</>
+						)}
+					</PaginationContent>
+				</Pagination>
+			)}
 		</div>
 	);
 }
