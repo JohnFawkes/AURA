38103260f03b14bd84cad145a8b9abe1fdfe10de
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-06-15 22:40:07 -0400

    Refactor JSONResponse structure and error handling in response functions for improved clarity and consistency

--- backend/internal/utils/response.go
+++ backend/internal/utils/response.go
@@ -3,67 +3,44 @@ package utils
 import (
 	"aura/internal/logging"
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strings"
 )
 
 type JSONResponse struct {
-	Status  string `json:"status,omitempty"`
-	Message string `json:"message,omitempty"`
-	Elapsed string `json:"elapsed,omitempty"`
-	Data    any    `json:"data,omitempty"`
+	Status  string                 `json:"status"`          // success, error, or warning
+	Elapsed string                 `json:"elapsed"`         // Time taken to process request
+	Data    any                    `json:"data,omitempty"`  // Response data (if any)
+	Error   *logging.StandardError `json:"error,omitempty"` // Error details (if any)
 }
 
-type SSEMessage struct {
-	Response JSONResponse `json:"response,omitempty"`
-	Progress SSEProgress  `json:"progress,omitempty"`
-}
-
-type SSEProgress struct {
-	Value    int    `json:"value,omitempty"`
-	Text     string `json:"text,omitempty"`
-	NextStep string `json:"nextStep,omitempty"`
-}
-
-func SendErrorJSONResponse(w http.ResponseWriter, statusCode int, err logging.ErrorLog) {
-	SendJsonResponse(w, statusCode, JSONResponse{
+func SendErrorResponse(w http.ResponseWriter, elapsed string, err logging.StandardError) {
+	if err.Function == "" {
+		err.Function = GetFunctionName()
+	}
+	if err.LineNumber == 0 {
+		err.LineNumber = GetLineNumber()
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	response := JSONResponse{
 		Status:  "error",
-		Message: fmt.Sprintf("%s - %s", err.Log.Message, err.Err.Error()),
-		Elapsed: err.Log.Elapsed,
-		Data:    nil,
-	})
+		Elapsed: elapsed,
+		Error:   &err,
+	}
+	// Log the error using the logging package
+	logging.LOG.Error(err.Message)
+	// Encode the response as JSON and send it
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		// Handle encoding error if necessary
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func SendJsonResponse(w http.ResponseWriter, statusCode int, response JSONResponse) {
-	// Set the Content-Type header to application/json
+
 	w.Header().Set("Content-Type", "application/json")
-
-	// Set the status code
-	w.WriteHeader(statusCode)
-
-	// Set the status field based on the status code if it's not already set
-	if response.Status == "" {
-		if statusCode == http.StatusOK {
-			response.Status = "success"
-		} else {
-			response.Status = "error"
-			if response.Message == "" {
-				response.Message = http.StatusText(statusCode)
-			}
-
-		}
-	}
-
-	// If there is an error, log it
-	if response.Status == "error" {
-		logging.LOG.Error(response.Message)
-	}
-
-	// Make the first letter of the message uppercase
-	if response.Message != "" {
-		response.Message = strings.ToUpper(string(response.Message[0])) + response.Message[1:]
-	}
+	w.WriteHeader(http.StatusOK)
 
 	// Encode the data into JSON and send it
 	if err := json.NewEncoder(w).Encode(response); err != nil {
@@ -71,15 +48,4 @@ func SendJsonResponse(w http.ResponseWriter, statusCode int, response JSONRespon
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
-}
-
-func SendSSEResponse(w http.ResponseWriter, flusher http.Flusher, message SSEMessage) {
-	jsonData, err := json.Marshal(message)
-	if err != nil {
-		logging.LOG.Error("Failed to marshal SSE message")
-		return
-	}
-	fmt.Fprintf(w, "data: %s\n\n", jsonData)
-	flusher.Flush()
 }
