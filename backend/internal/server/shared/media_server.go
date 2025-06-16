44ed4cb877620d50111926b0ff08d6bb8e20d091
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-06-15 22:39:30 -0400

    Refactor error handling in MediaServer interface to utilize StandardError for consistency and improved clarity

--- backend/internal/server/shared/media_server.go
+++ backend/internal/server/shared/media_server.go
@@ -18,187 +18,191 @@ import (
 type MediaServer interface {
 
 	// Get Status of the Media Server
-	GetMediaServerStatus() (string, logging.ErrorLog)
+	GetMediaServerStatus() (string, logging.StandardError)
 
 	// Get the library section info
-	FetchLibrarySectionInfo(library *modals.Config_MediaServerLibrary) (bool, logging.ErrorLog)
+	FetchLibrarySectionInfo(library *modals.Config_MediaServerLibrary) (bool, logging.StandardError)
 
 	// Get the library section items
-	FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex string) ([]modals.MediaItem, int, logging.ErrorLog)
+	FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex string) ([]modals.MediaItem, int, logging.StandardError)
 
 	// Get an item's content by Rating Key/ID
-	FetchItemContent(ratingKey string, sectionTitle string) (modals.MediaItem, logging.ErrorLog)
+	FetchItemContent(ratingKey string, sectionTitle string) (modals.MediaItem, logging.StandardError)
 
 	// Get an image from the media server
-	FetchImageFromMediaServer(ratingKey, imageType string) ([]byte, logging.ErrorLog)
+	FetchImageFromMediaServer(ratingKey, imageType string) ([]byte, logging.StandardError)
 
 	// Use the set to update the item on the media server
-	DownloadAndUpdatePosters(mediaItem modals.MediaItem, file modals.PosterFile) logging.ErrorLog
+	DownloadAndUpdatePosters(mediaItem modals.MediaItem, file modals.PosterFile) logging.StandardError
 
 	// Use the TMDB ID, type, title and library section to search for the item on the media server
-	SearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection string) (string, logging.ErrorLog)
+	SearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection string) (string, logging.StandardError)
 }
 
 type PlexServer struct{}
 type EmbyJellyServer struct{}
 
-func (p *PlexServer) GetMediaServerStatus() (string, logging.ErrorLog) {
+func (p *PlexServer) GetMediaServerStatus() (string, logging.StandardError) {
 	// Get the status of the Plex server
-	version, logErr := plex.GetMediaServerStatus()
-	if logErr.Err != nil {
-		return "", logErr
+	version, Err := plex.GetMediaServerStatus()
+	if Err.Message != "" {
+		return "", Err
 	}
-	return version, logging.ErrorLog{}
+	return version, logging.StandardError{}
 }
 
-func (e *EmbyJellyServer) GetMediaServerStatus() (string, logging.ErrorLog) {
+func (e *EmbyJellyServer) GetMediaServerStatus() (string, logging.StandardError) {
 	//Get the status of the Emby/Jellyfin server
-	version, logErr := emby_jellyfin.GetMediaServerStatus()
-	if logErr.Err != nil {
-		return "", logErr
+	version, Err := emby_jellyfin.GetMediaServerStatus()
+	if Err.Message != "" {
+		return "", Err
 	}
-	return version, logging.ErrorLog{}
+	return version, logging.StandardError{}
 }
 
-func (p *PlexServer) FetchLibrarySectionInfo(library *modals.Config_MediaServerLibrary) (bool, logging.ErrorLog) {
+func (p *PlexServer) FetchLibrarySectionInfo(library *modals.Config_MediaServerLibrary) (bool, logging.StandardError) {
 	// Fetch the library section from Plex
-	found, logErr := plex.FetchLibrarySectionInfo(library)
-	if logErr.Err != nil {
-		return false, logErr
+	found, Err := plex.FetchLibrarySectionInfo(library)
+	if Err.Message != "" {
+		return false, Err
 	}
-	return found, logging.ErrorLog{}
+	return found, logging.StandardError{}
 }
 
-func (e *EmbyJellyServer) FetchLibrarySectionInfo(library *modals.Config_MediaServerLibrary) (bool, logging.ErrorLog) {
+func (e *EmbyJellyServer) FetchLibrarySectionInfo(library *modals.Config_MediaServerLibrary) (bool, logging.StandardError) {
 	// Fetch the library section from Emby/Jellyfin
-	found, logErr := emby_jellyfin.FetchLibrarySectionInfo(library)
-	if logErr.Err != nil {
-		return false, logErr
+	found, Err := emby_jellyfin.FetchLibrarySectionInfo(library)
+	if Err.Message != "" {
+		return false, Err
 	}
-	return found, logging.ErrorLog{}
+	return found, logging.StandardError{}
 }
 
-func (p *PlexServer) FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex string) ([]modals.MediaItem, int, logging.ErrorLog) {
+func (p *PlexServer) FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex string) ([]modals.MediaItem, int, logging.StandardError) {
 	// Fetch the section content from Plex
-	mediaItems, totalSize, logErr := plex.FetchLibrarySectionItems(section, sectionStartIndex)
-	if logErr.Err != nil {
-		return nil, 0, logErr
+	mediaItems, totalSize, Err := plex.FetchLibrarySectionItems(section, sectionStartIndex, "500")
+	if Err.Message != "" {
+		return nil, 0, Err
 	}
-	return mediaItems, totalSize, logging.ErrorLog{}
+	return mediaItems, totalSize, logging.StandardError{}
 }
 
-func (e *EmbyJellyServer) FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex string) ([]modals.MediaItem, int, logging.ErrorLog) {
+func (e *EmbyJellyServer) FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex string) ([]modals.MediaItem, int, logging.StandardError) {
 	// Fetch the section content from Emby/Jellyfin
-	mediaItems, totalSize, logErr := emby_jellyfin.FetchLibrarySectionItems(section, sectionStartIndex)
-	if logErr.Err != nil {
-		return nil, 0, logErr
+	mediaItems, totalSize, Err := emby_jellyfin.FetchLibrarySectionItems(section, sectionStartIndex, "500")
+	if Err.Message != "" {
+		return nil, 0, Err
 	}
-	return mediaItems, totalSize, logging.ErrorLog{}
+	return mediaItems, totalSize, logging.StandardError{}
 }
 
-func (p *PlexServer) FetchItemContent(ratingKey string, sectionTitle string) (modals.MediaItem, logging.ErrorLog) {
+func (p *PlexServer) FetchItemContent(ratingKey string, sectionTitle string) (modals.MediaItem, logging.StandardError) {
 	// Fetch the item content from Plex
-	itemInfo, logErr := plex.FetchItemContent(ratingKey)
-	if logErr.Err != nil {
-		return itemInfo, logErr
+	itemInfo, Err := plex.FetchItemContent(ratingKey)
+	if Err.Message != "" {
+		return itemInfo, Err
 	}
-	return itemInfo, logging.ErrorLog{}
+	return itemInfo, logging.StandardError{}
 }
 
-func (e *EmbyJellyServer) FetchItemContent(ratingKey string, sectionTitle string) (modals.MediaItem, logging.ErrorLog) {
+func (e *EmbyJellyServer) FetchItemContent(ratingKey string, sectionTitle string) (modals.MediaItem, logging.StandardError) {
 	// Fetch the item content from Emby/Jellyfin
-	itemInfo, logErr := emby_jellyfin.FetchItemContent(ratingKey, sectionTitle)
-	if logErr.Err != nil {
-		return itemInfo, logErr
+	itemInfo, Err := emby_jellyfin.FetchItemContent(ratingKey, sectionTitle)
+	if Err.Message != "" {
+		return itemInfo, Err
 	}
-	return itemInfo, logging.ErrorLog{}
+	return itemInfo, logging.StandardError{}
 }
 
-func (p *PlexServer) FetchImageFromMediaServer(ratingKey, imageType string) ([]byte, logging.ErrorLog) {
+func (p *PlexServer) FetchImageFromMediaServer(ratingKey, imageType string) ([]byte, logging.StandardError) {
 	// Fetch the image from Plex
-	imageData, logErr := plex.FetchImageFromMediaServer(ratingKey, imageType)
-	if logErr.Err != nil {
-		return nil, logErr
+	imageData, Err := plex.FetchImageFromMediaServer(ratingKey, imageType)
+	if Err.Message != "" {
+		return nil, Err
 	}
-	return imageData, logging.ErrorLog{}
+	return imageData, logging.StandardError{}
 }
 
-func (e *EmbyJellyServer) FetchImageFromMediaServer(ratingKey, imageType string) ([]byte, logging.ErrorLog) {
+func (e *EmbyJellyServer) FetchImageFromMediaServer(ratingKey, imageType string) ([]byte, logging.StandardError) {
 	// Fetch the image from Emby/Jellyfin
-	imageData, logErr := emby_jellyfin.FetchImageFromMediaServer(ratingKey, imageType)
-	if logErr.Err != nil {
-		return nil, logErr
+	imageData, Err := emby_jellyfin.FetchImageFromMediaServer(ratingKey, imageType)
+	if Err.Message != "" {
+		return nil, Err
 	}
-	return imageData, logging.ErrorLog{}
+	return imageData, logging.StandardError{}
 }
 
-func (p *PlexServer) DownloadAndUpdatePosters(mediaItem modals.MediaItem, file modals.PosterFile) logging.ErrorLog {
+func (p *PlexServer) DownloadAndUpdatePosters(mediaItem modals.MediaItem, file modals.PosterFile) logging.StandardError {
 	// Download and update the item on Plex
-	logErr := plex.DownloadAndUpdatePosters(mediaItem, file)
-	if logErr.Err != nil {
-		return logErr
+	Err := plex.DownloadAndUpdatePosters(mediaItem, file)
+	if Err.Message != "" {
+		return Err
 	}
-	return logging.ErrorLog{}
+	return logging.StandardError{}
 }
 
-func (e *EmbyJellyServer) DownloadAndUpdatePosters(mediaItem modals.MediaItem, file modals.PosterFile) logging.ErrorLog {
+func (e *EmbyJellyServer) DownloadAndUpdatePosters(mediaItem modals.MediaItem, file modals.PosterFile) logging.StandardError {
 	// Download and update the item on Emby/Jellyfin
-	logErr := emby_jellyfin.DownloadAndUpdatePosters(mediaItem, file)
-	if logErr.Err != nil {
-		return logErr
+	Err := emby_jellyfin.DownloadAndUpdatePosters(mediaItem, file)
+	if Err.Message != "" {
+		return Err
 	}
-	return logging.ErrorLog{}
+	return logging.StandardError{}
 }
 
-func (p *PlexServer) SearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection string) (string, logging.ErrorLog) {
+func (p *PlexServer) SearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection string) (string, logging.StandardError) {
 	// Search for the item on Plex
-	ratingKey, logErr := mediux.PlexSearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection)
-	if logErr.Err != nil {
-		return ratingKey, logErr
+	ratingKey, Err := mediux.PlexSearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection)
+	if Err.Message != "" {
+		return ratingKey, Err
 	}
-	return ratingKey, logging.ErrorLog{}
+	return ratingKey, logging.StandardError{}
 }
 
-func (e *EmbyJellyServer) SearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection string) (string, logging.ErrorLog) {
+func (e *EmbyJellyServer) SearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection string) (string, logging.StandardError) {
 	// Search for the item on Emby/Jellyfin
-	ratingKey, logErr := mediux.EmbyJellySearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection)
-	if logErr.Err != nil {
-		return ratingKey, logErr
+	ratingKey, Err := mediux.EmbyJellySearchForItemAndGetRatingKey(tmdbID, itemType, itemTitle, librarySection)
+	if Err.Message != "" {
+		return ratingKey, Err
 	}
-	return ratingKey, logging.ErrorLog{}
+	return ratingKey, logging.StandardError{}
 }
 
-func InitUserID() logging.ErrorLog {
+func InitUserID() logging.StandardError {
 	if config.Global.MediaServer.Type == "Plex" {
-		return logging.ErrorLog{}
+		return logging.StandardError{}
 	}
+
+	Err := logging.NewStandardError()
 
 	// Parse the base URL
 	baseURL, err := url.Parse(config.Global.MediaServer.URL)
 	if err != nil {
-		return logging.ErrorLog{Err: err, Log: logging.Log{Message: "Invalid base URL"}}
+
+		Err.Message = "Failed to parse base URL"
+		Err.HelpText = "Ensure the Media Server URL is correctly configured in the settings."
+		Err.Details = fmt.Sprintf("Base URL: %s", config.Global.MediaServer.URL)
+		return Err
 	}
 	// Construct the full URL by appending the path
 	baseURL.Path = path.Join(baseURL.Path, "Users")
 	url := baseURL.String()
 
 	// Make a GET request to the Emby server
-	response, body, logErr := utils.MakeHTTPRequest(url, http.MethodGet, nil, 60, nil, "MediaServer")
-	if logErr.Err != nil {
-		return logErr
+	response, body, Err := utils.MakeHTTPRequest(url, http.MethodGet, nil, 60, nil, "MediaServer")
+	if Err.Message != "" {
+		return Err
 	}
 	defer response.Body.Close()
-
-	// Check if the response status is OK
-	if response.StatusCode != http.StatusOK {
-		return logging.ErrorLog{Err: fmt.Errorf("bad status code"),
-			Log: logging.Log{Message: fmt.Sprintf("Received status code '%d' from %s server", response.StatusCode, config.Global.MediaServer.Type)}}
-	}
 
 	var responseSection modals.EmbyJellyUserIDResponse
 	err = json.Unmarshal(body, &responseSection)
 	if err != nil {
-		return logging.ErrorLog{Err: err, Log: logging.Log{Message: "Failed to parse JSON response"}}
+
+		Err.Message = "Failed to parse Emby/Jellyfin user ID response"
+		Err.HelpText = "Ensure the Emby/Jellyfin server is returning a valid JSON response."
+		Err.Details = fmt.Sprintf("Error: %s", err.Error())
+		return Err
 	}
 
 	// Find the first Admin user ID
@@ -207,10 +211,15 @@ func InitUserID() logging.ErrorLog {
 			config.Global.MediaServer.UserID = user.ID
 			maskedUserID := fmt.Sprintf("****%s", user.ID[len(user.ID)-7:])
 			logging.LOG.Debug(fmt.Sprintf("Found Admin user ID: %s", maskedUserID))
-			return logging.ErrorLog{}
+			return logging.StandardError{}
 		}
 	}
 
-	return logging.ErrorLog{Err: fmt.Errorf("no admin user found"),
-		Log: logging.Log{Message: "No Admin user found"}}
+	// If no Admin user is found, return an error
+
+	Err.Message = "No Admin user found in Emby/Jellyfin user list"
+	Err.HelpText = "Ensure that there is at least one user with Admin privileges in the Emby/Jellyfin server."
+	Err.Details = "No Admin user found in the Emby/Jellyfin user list"
+	return Err
+
 }
