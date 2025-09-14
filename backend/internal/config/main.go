63a7afc3188af627a70b6c5f50b4b6e20a407add
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-09-14 00:26:20 -0400

    feat: add initial configuration setup with global instance and validation flags

---
+++ backend/internal/config/main.go
@@ -0,0 +1,11 @@
+package config
+
+import "aura/internal/modals"
+
+// Global is a pointer to the global configuration instance.
+// It is used throughout the application to access configuration settings.
+var Global *modals.Config = &modals.Config{}
+
+// By default, the configuration is invalid until it is validated.
+var ConfigLoaded bool = false
+var ConfigValid bool = false
