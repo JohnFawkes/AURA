90bb73ca001d00b338854c4f89caf66921ce18dd
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-09-14 16:57:50 -0400

    [BUG] Opening a Jellyfin collection in AURA displays a page with an error Fixes #73

--- backend/internal/server/emby_jellyfin/fetch_library_section_items.go
+++ backend/internal/server/emby_jellyfin/fetch_library_section_items.go
@@ -49,7 +49,6 @@ func FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex s
 	var responseSection modals.EmbyJellyLibraryItemsResponse
 	err := json.Unmarshal(body, &responseSection)
 	if err != nil {
-
 		Err.Message = "Failed to parse JSON response"
 		Err.HelpText = "Ensure the Emby/Jellyfin server is returning a valid JSON response."
 		Err.Details = fmt.Sprintf("Error: %s", err.Error())
@@ -58,7 +57,6 @@ func FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex s
 
 	// Check to see if any items were returned
 	if len(responseSection.Items) == 0 {
-
 		Err.Message = "No items found in the library section"
 		Err.HelpText = fmt.Sprintf("Ensure the library section '%s' has items.", section.Title)
 		Err.Details = fmt.Sprintf("No items found for section ID '%s' with title '%s'.", section.ID, section.Title)
@@ -68,6 +66,19 @@ func FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex s
 	var items []modals.MediaItem
 	for _, item := range responseSection.Items {
 		var itemInfo modals.MediaItem
+
+		// If Type is Boxset, then split them up
+		if item.Type == "BoxSet" {
+			// Split the BoxSet into individual items
+			boxSetItems, boxSetErr := SplitCollectionIntoIndividualItems(item.Name, item.ID, section.Title)
+			if boxSetErr.Message != "" {
+				logging.LOG.Error(boxSetErr.Message)
+				return nil, 0, boxSetErr
+			}
+			items = append(items, boxSetItems...)
+			continue
+		}
+
 		itemInfo.RatingKey = item.ID
 		itemInfo.Type = map[string]string{
 			"Movie":  "movie",
@@ -94,4 +105,81 @@ func FetchLibrarySectionItems(section modals.LibrarySection, sectionStartIndex s
 	}
 
 	return items, responseSection.TotalRecordCount, logging.StandardError{}
+}
+
+func SplitCollectionIntoIndividualItems(collectionName, parentID, sectionTitle string) ([]modals.MediaItem, logging.StandardError) {
+	logging.LOG.Trace(fmt.Sprintf("Splitting '%s' into individual items (ID: %s)", collectionName, parentID))
+	Err := logging.NewStandardError()
+
+	baseURL, Err := utils.MakeMediaServerAPIURL(fmt.Sprintf("Users/%s/Items", config.Global.MediaServer.UserID), config.Global.MediaServer.URL)
+	if Err.Message != "" {
+		return nil, Err
+	}
+
+	// Add query parameters
+	params := url.Values{}
+	params.Add("Recursive", "true")
+	params.Add("SortBy", "Name")
+	params.Add("SortOrder", "Ascending")
+	params.Add("IncludeItemTypes", "Movie,Series,BoxSet")
+	params.Add("Fields", "DateLastContentAdded,PremiereDate,DateCreated,ProviderIds,BasicSyncInfo,CanDelete,CanDownload,PrimaryImageAspectRatio,ProductionYear,Status,EndDate")
+	params.Add("ParentId", parentID)
+
+	baseURL.RawQuery = params.Encode()
+
+	// Make a GET request to the Emby server
+	response, body, Err := utils.MakeHTTPRequest(baseURL.String(), http.MethodGet, nil, 60, nil, "MediaServer")
+	if Err.Message != "" {
+		logging.LOG.Error(Err.Message)
+		return nil, Err
+	}
+	defer response.Body.Close()
+
+	var responseSection modals.EmbyJellyLibraryItemsResponse
+	err := json.Unmarshal(body, &responseSection)
+	if err != nil {
+		Err.Message = "Failed to parse JSON response"
+		Err.HelpText = "Ensure the Emby/Jellyfin server is returning a valid JSON response."
+		Err.Details = fmt.Sprintf("Error: %s", err.Error())
+		return nil, Err
+	}
+
+	// Check to see if any items were returned
+	if len(responseSection.Items) == 0 {
+		Err.Message = "No items found in the library section"
+		Err.HelpText = fmt.Sprintf("Ensure the library section '%s' has items.", sectionTitle)
+		Err.Details = fmt.Sprintf("No items found for section ID '%s' with title '%s'.", parentID, sectionTitle)
+		return nil, Err
+	}
+
+	var items []modals.MediaItem
+	for _, item := range responseSection.Items {
+		var itemInfo modals.MediaItem
+
+		itemInfo.RatingKey = item.ID
+		itemInfo.Type = map[string]string{
+			"Movie":  "movie",
+			"Series": "show",
+		}[item.Type]
+		itemInfo.Title = item.Name
+		itemInfo.Year = item.ProductionYear
+		itemInfo.Thumb = item.ImageTags.Thumb
+		itemInfo.LibraryTitle = sectionTitle
+		if item.ProviderIds.Tmdb != "" {
+			itemInfo.Guids = append(itemInfo.Guids, modals.Guid{Provider: "tmdb", ID: item.ProviderIds.Tmdb})
+		}
+		itemInfo.AddedAt = item.DateCreated.UnixMilli()
+		itemInfo.ReleasedAt = item.PremiereDate.UnixMilli()
+		existsInDB, _ := database.CheckIfMediaItemExistsInDatabase(item.ID)
+
+		if existsInDB {
+			itemInfo.ExistInDatabase = true
+		} else {
+			itemInfo.ExistInDatabase = false
+		}
+
+		items = append(items, itemInfo)
+	}
+
+	return items, Err
 }
