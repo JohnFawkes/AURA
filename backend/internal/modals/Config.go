0d735d5761ec78ee500581fdd62a8620e8c1435c
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-09-14 00:27:04 -0400

    refactor: update configuration struct fields for consistency and clarity

--- backend/internal/modals/Config.go
+++ backend/internal/modals/Config.go
@@ -4,72 +4,76 @@ package modals
 // It includes options for server setup, caching, data storage, logging,
 // and integration with external services such as Plex, TMDB, and Mediux.
 type Config struct {
-	Auth                   Config_Auth          `yaml:"Auth"`                   // Authentication settings.
-	Dev                    Config_Dev           `yaml:"Dev"`                    // Development mode settings.
-	CacheImages            bool                 `yaml:"CacheImages"`            // Whether to cache images locally.
-	SaveImageNextToContent bool                 `yaml:"SaveImageNextToContent"` // Whether to save images next to the associated content.
-	Logging                Config_Logging       `yaml:"Logging"`                // Logging configuration settings.
-	MediaServer            Config_MediaServer   `yaml:"MediaServer"`            // Media server integration settings.
-	TMDB                   Config_TMDB          `yaml:"TMDB"`                   // TMDB (The Movie Database) integration settings.
-	Mediux                 Config_Mediux        `yaml:"Mediux"`                 // Mediux integration settings.
-	AutoDownload           Config_AutoDownload  `yaml:"AutoDownload"`           // Auto-download settings.
-	Kometa                 Config_Kometa        `yaml:"Kometa"`                 // Kometa settings.
-	Notifications          Config_Notifications `yaml:"Notifications"`          // Notification settings.
-}
-
-type Config_Auth struct {
-	Enable   bool   `yaml:"Enable"`   // Whether to enable authentication.
-	Password string `yaml:"Password"` // Password for authentication.
+	Dev           Config_Dev           `yaml:"Dev,omitempty" json:"-"`  // Development mode settings.
+	Auth          Config_Auth          `yaml:"Auth,omitempty"`          // Authentication settings.
+	Logging       Config_Logging       `yaml:"Logging,omitempty"`       // Logging configuration settings.
+	MediaServer   Config_MediaServer   `yaml:"MediaServer,omitempty"`   // Media server integration settings.
+	Mediux        Config_Mediux        `yaml:"Mediux,omitempty"`        // Mediux integration settings.
+	AutoDownload  Config_AutoDownload  `yaml:"AutoDownload,omitempty"`  // Auto-download settings.
+	Images        Config_Images        `yaml:"Images,omitempty"`        // Image settings.
+	TMDB          Config_TMDB          `yaml:"TMDB,omitempty"`          // TMDB (The Movie Database) integration settings.
+	Kometa        Config_Kometa        `yaml:"Kometa,omitempty"`        // Kometa settings.
+	Notifications Config_Notifications `yaml:"Notifications,omitempty"` // Notification settings.
 }
 
 type Config_Dev struct {
-	Enable    bool   `yaml:"Enable"`    // Whether to enable development mode.
-	LocalPath string `yaml:"LocalPath"` // Local path for development mode.
+	Enabled   bool   `yaml:"Enabled,omitempty"`   // Whether to enable development mode.
+	LocalPath string `yaml:"LocalPath,omitempty"` // Local path for development mode.
+}
+type Config_Auth struct {
+	Enabled  bool   `yaml:"Enabled"`            // Whether to enable authentication.
+	Password string `yaml:"Password,omitempty"` // Password for authentication.
 }
 
-// Config_Logging represents the logging configuration settings.
 type Config_Logging struct {
-	Level string `yaml:"Level"` // Logging level (e.g., TRACE, DEBUG, INFO, WARN, ERROR).
-	File  string `yaml:"File"`  // File path for logging output.
+	Level string `yaml:"Level"`          // Logging level (e.g., TRACE, DEBUG, INFO, WARN, ERROR).
+	File  string `yaml:"File,omitempty"` // File path for logging output.
 }
 
-// Config_MediaServerLibrary represents a single media server library configuration.
-type Config_MediaServerLibrary struct {
-	Name      string `yaml:"Name"`      // Name of the library.
-	SectionID string `yaml:"SectionID"` // Unique identifier for the library section.
-	Type      string `yaml:"Type"`      // Type of the library (e.g., movie, show). All other types are ignored.
-}
-
-// Config_MediaServer represents the configuration for media server integration.
 type Config_MediaServer struct {
-	Type                   string                      `yaml:"Type"`                   // Type of media server (e.g., plex, emby, jellyfin).
-	URL                    string                      `yaml:"URL"`                    // Base URL of the media server. This is either the IP:Port or the domain name (e.g., plex.domain.com).
-	Token                  string                      `yaml:"Token"`                  // Authentication token for accessing the media server.
-	Libraries              []Config_MediaServerLibrary `yaml:"Libraries"`              // List of media server libraries to manage.
-	SeasonNamingConvention string                      `yaml:"SeasonNamingConvention"` // Season naming convention for the media server. Only needed for Plex. Will default to 2
-	UserID                 string                      `yaml:"UserID"`                 // User ID for accessing the media server. This is used for Emby and Jellyfin servers.
+	Type                   string                      `yaml:"Type"`                             // Type of media server (e.g., plex, emby, jellyfin).
+	URL                    string                      `yaml:"URL"`                              // Base URL of the media server. This is either the IP:Port or the domain name (e.g., plex.domain.com).
+	Token                  string                      `yaml:"Token"`                            // Authentication token for accessing the media server.
+	Libraries              []Config_MediaServerLibrary `yaml:"Libraries,omitempty"`              // List of media server libraries to manage.
+	SeasonNamingConvention string                      `yaml:"SeasonNamingConvention,omitempty"` // Season naming convention for the media server. Only needed for Plex. Will default to 2
+	UserID                 string                      `yaml:"UserID,omitempty"`                 // User ID for accessing the media server. This is used for Emby and Jellyfin servers.
+}
+type Config_MediaServerLibrary struct {
+	Name      string `yaml:"Name,omitempty"`      // Name of the library.
+	SectionID string `yaml:"SectionID,omitempty"` // Unique identifier for the library section.
+	Type      string `yaml:"Type,omitempty"`      // Type of the library (e.g., movie, show). All other types are ignored.
 }
 
-// Config_TMDB represents the configuration for TMDB (The Movie Database) integration.
-type Config_TMDB struct {
-	ApiKey string `yaml:"ApiKey"` // API key for accessing TMDB services.
-}
-
-// Config_Mediux represents the configuration for Mediux integration.
 type Config_Mediux struct {
 	Token           string `yaml:"Token"`           // Authentication token for accessing Mediux services.
 	DownloadQuality string `yaml:"DownloadQuality"` // Quality of the media to download from Mediux (Options: "original", "optimized") Defaults to "optimized".
 }
 
-// Config_AutoDownload represents the configuration for auto-download settings.
 type Config_AutoDownload struct {
-	Enabled bool   `yaml:"Enabled"` // Whether auto-download is enabled.
-	Cron    string `yaml:"Cron"`    // Cron expression for scheduling auto-downloads.
+	Enabled bool   `yaml:"Enabled"`        // Whether auto-download is enabled.
+	Cron    string `yaml:"Cron,omitempty"` // Cron expression for scheduling auto-downloads.
+}
+
+type Config_Images struct {
+	CacheImages            Config_CacheImages            `yaml:"CacheImages"`            // Settings for caching images.
+	SaveImageNextToContent Config_SaveImageNextToContent `yaml:"SaveImageNextToContent"` // Settings for saving images next to their content.
+}
+
+type Config_CacheImages struct {
+	Enabled bool `yaml:"Enabled"` // Whether to enable caching of images.
+}
+
+type Config_SaveImageNextToContent struct {
+	Enabled bool `yaml:"Enabled"` // Whether to save images next to their content.
+}
+
+type Config_TMDB struct {
+	ApiKey string `yaml:"ApiKey,omitempty" json:"-"` // API key for accessing TMDB services.
 }
 
 type Config_Kometa struct {
-	RemoveLabels bool     `yaml:"RemoveLabels"` // Whether to remove overlays from images.
-	Labels       []string `yaml:"Labels"`       // List of labels to remove from images.
+	RemoveLabels bool     `yaml:"RemoveLabels"`     // Whether to remove overlays from images.
+	Labels       []string `yaml:"Labels,omitempty"` // List of labels to remove from images.
 }
 
 type Config_Notifications struct {
@@ -78,17 +82,17 @@ type Config_Notifications struct {
 }
 
 type Config_Notification_Providers struct {
-	Provider string                        `yaml:"Provider"`           // Notification provider
-	Enabled  bool                          `yaml:"Enabled"`            // Whether this notification method is enabled
+	Provider string                        `yaml:"Provider,omitempty"` // Notification provider
+	Enabled  bool                          `yaml:"Enabled,omitempty"`  // Whether this notification method is enabled
 	Discord  *Config_Notification_Discord  `yaml:"Discord,omitempty"`  // Discord notification settings
 	Pushover *Config_Notification_Pushover `yaml:"Pushover,omitempty"` // Pushover notification settings
 }
 
 type Config_Notification_Discord struct {
-	Webhook string `yaml:"Webhook"` // Webhook URL for the Discord notification provider.
+	Webhook string `yaml:"Webhook,omitempty"` // Webhook URL for the Discord notification provider.
 }
 
 type Config_Notification_Pushover struct {
-	Token   string `yaml:"Token"`   // Token for the Pushover notification provider.
-	UserKey string `yaml:"UserKey"` // UserKey for the Pushover notification provider.
+	Token   string `yaml:"Token,omitempty"`   // Token for the Pushover notification provider.
+	UserKey string `yaml:"UserKey,omitempty"` // UserKey for the Pushover notification provider.
 }
