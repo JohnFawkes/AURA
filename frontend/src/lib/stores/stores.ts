6db7c9e0f59cfcfe63bfdf3560ae581132815b78
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-09-21 01:59:27 -0400

    feat: add driver property to localforage instances for consistency

--- frontend/src/lib/stores/stores.ts
+++ frontend/src/lib/stores/stores.ts
@@ -4,10 +4,12 @@ export const PageStore = localforage.createInstance({
 	name: "aura",
 	storeName: "PageStores",
 	version: 1.0,
+	driver: localforage.INDEXEDDB,
 });
 
 export const GlobalStore = localforage.createInstance({
 	name: "aura",
 	storeName: "GlobalStores",
 	version: 1.0,
+	driver: localforage.INDEXEDDB,
 });
