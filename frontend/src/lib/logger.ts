56dbf1ddb994d77b78afa3043ddc94ee0429c1d9
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-09-21 01:59:34 -0400

    feat: enhance logging to include debug mode for development environment

--- frontend/src/lib/logger.ts
+++ frontend/src/lib/logger.ts
@@ -1,5 +1,6 @@
 export const log = (...args: unknown[]) => {
-	const debugMode = localStorage.getItem("debugMode") === "true";
+	const debugMode =
+		localStorage.getItem("debugMode") === "true" || process.env.NEXT_PUBLIC_APP_VERSION?.endsWith("dev");
 	if (debugMode) {
 		// eslint-disable-next-line no-console
 		console.log(...args);
