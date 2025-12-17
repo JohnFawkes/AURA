42dcdabccbfe9567b502b300e4bb535511fbbc18
Ahmad M <handbag-rhyme-0j@icloud.com>
2025-12-16 20:59:31 -0500

    feat: Allow custom trigger for PopoverHelp component

--- frontend/src/components/shared/popover-help.tsx
+++ frontend/src/components/shared/popover-help.tsx
@@ -7,6 +7,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 
 interface PopoverHelpProps {
 	children: ReactNode;
+	trigger?: ReactNode;
 	ariaLabel: string;
 	side?: "top" | "right" | "bottom" | "left";
 	align?: "start" | "center" | "end";
@@ -16,6 +17,7 @@ interface PopoverHelpProps {
 
 export function PopoverHelp({
 	children,
+	trigger,
 	ariaLabel,
 	side = "bottom",
 	align = "center",
@@ -25,13 +27,17 @@ export function PopoverHelp({
 	return (
 		<Popover>
 			<PopoverTrigger asChild>
-				<Button
-					variant="outline"
-					className="h-6 w-6 rounded-md border flex items-center justify-center text-xs bg-background hover:bg-muted transition hover:cursor-help active:scale-95 hover:brightness-120 p-0"
-					aria-label={ariaLabel}
-				>
-					<HelpCircle className="h-4 w-4" />
-				</Button>
+				{trigger ? (
+					trigger
+				) : (
+					<Button
+						variant="outline"
+						className="h-6 w-6 rounded-md border flex items-center justify-center text-xs bg-background hover:bg-muted transition hover:cursor-help active:scale-95 hover:brightness-120 p-0"
+						aria-label={ariaLabel}
+					>
+						<HelpCircle className="w-4 h-4" />
+					</Button>
+				)}
 			</PopoverTrigger>
 			<PopoverContent side={side} align={align} sideOffset={sideOffset} className={className}>
 				{children}
