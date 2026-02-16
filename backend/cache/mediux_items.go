3b21ffaf704bfe34c29d87f1f05a364b1ec6e5c6
Ahmad M <handbag-rhyme-0j@icloud.com>
2026-02-16 16:39:19 -0500

    feat: new cache to hold items from mediux that have sets

---
+++ backend/cache/mediux_items.go
@@ -0,0 +1,89 @@
+package cache
+
+import (
+	"aura/models"
+	"sync"
+)
+
+var MediuxItems *MediuxItemCache
+
+type MediuxItemCache struct {
+	items map[string][]*models.MediuxContentID // Key: ItemType, Value: slice of TMDBIDs
+	mu    sync.RWMutex
+}
+
+func NewMediuxItemCache() *MediuxItemCache {
+	return &MediuxItemCache{
+		items: make(map[string][]*models.MediuxContentID),
+	}
+}
+
+func init() {
+	MediuxItems = NewMediuxItemCache()
+}
+
+func (c *MediuxItemCache) StoreMediuxItems(items []models.MediuxContentID) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, value := range items {
+		c.items[value.Type] = append(c.items[value.Type], &models.MediuxContentID{
+			ID:   value.ID,
+			Type: value.Type,
+		})
+	}
+}
+
+func (c *MediuxItemCache) GetMediuxItems() []models.MediuxContentID {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	itemList := make([]models.MediuxContentID, 0)
+	for _, items := range c.items {
+		for _, item := range items {
+			itemList = append(itemList, *item)
+		}
+	}
+	return itemList
+}
+
+func (c *MediuxItemCache) CheckItemExists(itemType string, tmdbID string) bool {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	for _, item := range c.items[itemType] {
+		if item.ID == tmdbID {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *MediuxItemCache) AddItem(itemType string, tmdbID string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// Prevent duplicates
+	for _, item := range c.items[itemType] {
+		if item.ID == tmdbID {
+			return
+		}
+	}
+	c.items[itemType] = append(c.items[itemType], &models.MediuxContentID{
+		ID:   tmdbID,
+		Type: itemType,
+	})
+}
+
+func (c *MediuxItemCache) RemoveItem(itemType string, tmdbID string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	items := c.items[itemType]
+	for i, item := range items {
+		if item.ID == tmdbID {
+			c.items[itemType] = append(items[:i], items[i+1:]...)
+			break
+		}
+	}
+}
