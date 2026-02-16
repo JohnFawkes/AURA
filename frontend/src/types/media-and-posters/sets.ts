f3015de3880a97c0895c74b911d24c611f5545ef
Ahmad M <handbag-rhyme-0j@icloud.com>
2026-02-16 16:45:35 -0500

    feat: refactor media item interfaces and add new sets management structure

---
+++ frontend/src/types/media-and-posters/sets.ts
@@ -0,0 +1,71 @@
+import { MediaItem } from "@/types/media-and-posters/media-item-and-library";
+import { TYPE_DB_SET_TYPE_OPTIONS, TYPE_SET_TYPE_OPTIONS } from "@/types/ui-options";
+
+export interface BaseMediuxItemInfo {
+	tmdb_id: string;
+	type: string;
+	date_updated: string;
+	status: string;
+	title: string;
+	tagline: string;
+	release_date: string;
+	tvdb_id: string;
+	imdb_id: string;
+	trakt_id: string;
+	slug: string;
+	tmdb_poster_path: string;
+	tmdb_backdrop_path: string;
+}
+
+export interface BaseSetInfo {
+	id: string;
+	title: string;
+	type: TYPE_SET_TYPE_OPTIONS;
+	user_created: string;
+	date_created: string;
+	date_updated: string;
+	popularity: number;
+	popularity_global: number;
+}
+
+export interface ImageFile {
+	id: string;
+	type: string;
+	modified: string;
+	file_size: number;
+	src: string;
+	blurhash: string;
+	item_tmdb_id: string;
+	title?: string;
+	season_number?: number;
+	episode_number?: number;
+}
+
+export interface SetRef extends Omit<BaseSetInfo, "type"> {
+	type: TYPE_DB_SET_TYPE_OPTIONS;
+	images: ImageFile[];
+	item_ids: string[];
+}
+
+export interface BoxsetRef extends Omit<BaseSetInfo, "type"> {
+	type: "boxset";
+	set_ids: { [key: string]: string[] }; // IDs of sets (show, movie, collection) in this boxset
+}
+
+export interface IncludedItem {
+	mediux_info: BaseMediuxItemInfo;
+	media_item: MediaItem;
+}
+
+export interface PosterSetsResponse {
+	sets: SetRef[];
+	included_items: { [tmdb_id: string]: IncludedItem };
+}
+
+export interface CreatorSetsResponse {
+	show_sets: SetRef[];
+	movie_sets: SetRef[];
+	collection_sets: SetRef[];
+	boxsets: BoxsetRef[];
+	included_items: { [tmdb_id: string]: IncludedItem };
+}
