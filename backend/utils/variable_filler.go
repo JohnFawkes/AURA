3b5aec2cdd23a56cefd245e48f5506bb903456cb
Moose <handbag-rhyme-0j@icloud.com>
2026-03-14 17:13:20 -0400

    feat: enhance template variable handling for autodownload functionality

--- backend/utils/variable_filler.go
+++ backend/utils/variable_filler.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"aura/config"
+	"aura/models"
 	"fmt"
 	maps0 "maps"
 	"regexp"
@@ -41,25 +42,49 @@ func MergeTemplateVars(maps ...map[string]string) map[string]string {
 
 func BaseTemplateVars() map[string]string {
 	return map[string]string{
+		"AppName":         config.AppName,
+		"AppVersion":      config.AppVersion,
+		"AppPort":         fmt.Sprintf("%d", config.AppPort),
+		"AppAuthor":       config.AppAuthor,
+		"AppLicense":      config.AppLicense,
 		"MediaServerName": config.MediaServerName,
 		"MediaServerType": config.Current.MediaServer.Type,
 		"Timestamp":       time.Now().Format("2006-01-02 15:04:05"),
+		"NewLine":         "\n",
+		"Tab":             "\t",
 	}
 }
 
 func TemplateVars_AppStartup(appName, appVersion string, appPort int) map[string]string {
 	return MergeTemplateVars(
 		BaseTemplateVars(),
-		map[string]string{
-			"AppName":    appName,
-			"AppVersion": appVersion,
-			"AppPort":    fmt.Sprintf("%d", appPort),
-		},
 	)
 }
 
 func TemplateVars_TestNotification() map[string]string {
 	return MergeTemplateVars(
 		BaseTemplateVars(),
+	)
+}
+
+func TemplateVars_Autodownload(mediaItem models.MediaItem, setItem models.DBPosterSetDetail, image models.ImageFile, reasonTitle string, reason string) map[string]string {
+	return MergeTemplateVars(
+		BaseTemplateVars(),
+		map[string]string{
+			"MediaItemTitle":        mediaItem.Title,
+			"MediaItemYear":         fmt.Sprintf("%d", mediaItem.Year),
+			"MediaItemTMDBID":       mediaItem.TMDB_ID,
+			"MediaItemLibraryTitle": mediaItem.LibraryTitle,
+			"MediaItemRatingKey":    mediaItem.RatingKey,
+			"MediaItemType":         mediaItem.Type,
+			"SetID":                 setItem.ID,
+			"SetTitle":              setItem.Title,
+			"SetType":               setItem.Type,
+			"SetCreator":            setItem.UserCreated,
+			"ImageName":             GetFileDownloadName(mediaItem.Title, image),
+			"ImageType":             image.Type,
+			"ReasonTitle":           reasonTitle,
+			"Reason":                reason,
+		},
 	)
 }
